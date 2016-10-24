633be16c05ad923e3ae33b353fd079ee754a1540
Your Name <you@example.com>
2016-10-24 19:47:49 +0200

    Segon canvi, segueix reiniciant-se per raons desconegudes (carrega el programa be a la FPGA)

--- WEB/carregar_app.bat
+++ WEB/carregar_app.bat
@@ -17,30 +17,35 @@ GOTO CREATE_MFS
 
 REM crea la imatge per la pagina web
 :CREATE_MFS
-mfsgen -cvbf ./image.mfs 16 css index.html
+del "image.mfs"
+REM mfsgen -cvbf ./image.mfs 16 css index.html
+mfsgen -cvbf ./image.mfs 16 index.html
 GOTO DO_BATCH
 
 REM si ha detectat el ISE correctament i la imatge ja ha estat creada
 :DO_BATCH
-@echo setMode -bscan															>  configuracio_batch_impact.cmd
-@echo setCable -target "digilent_plugin DEVICE=SN:000000000000 FREQUENCY=-1"	>> configuracio_batch_impact.cmd
-@echo identify																	>> configuracio_batch_impact.cmd
-@echo assignfile -p 1 -file bootloop.bit										>> configuracio_batch_impact.cmd
-@echo program -p 1																>> configuracio_batch_impact.cmd
-@echo quit																		>> configuracio_batch_impact.cmd
+del "configuracio_batch_impact.cmd"
+@echo setMode -bscan																	>  configuracio_batch_impact.cmd
+@echo setCable -target "digilent_plugin DEVICE=SN:000000000000 FREQUENCY=-1"			>> configuracio_batch_impact.cmd
+@echo identify																			>> configuracio_batch_impact.cmd
+@echo assignfile -p 1 -file ..\SDK\SDK_Export\lwip_example_hw_platform\download.bit		>> configuracio_batch_impact.cmd
+@echo program -p 1																		>> configuracio_batch_impact.cmd
+@echo quit																				>> configuracio_batch_impact.cmd
 GOTO CONNECT
 
 REM un cop ja ha carregat el bootloop a la Spartan6
-
 REM connect mb mdm -port COMx -baud 115200 -timeout 5 -cable type digilent_plugin
 
 :CONNECT
 impact -batch configuracio_batch_impact.cmd
 sleep 3
+del "raw_apps.link"
+mklink raw_apps.link "..\SDK\SDK_Export\raw_apps\Debug\raw_apps.elf"
+del "xmd.ini"
 @echo connect mb mdm -cable type digilent_plugin	>  xmd.ini
 @echo rst											>> xmd.ini
 @echo dow -data image.mfs 0xA5000000				>> xmd.ini
-@echo dow raw_apps.elf								>> xmd.ini
+@echo dow raw_apps.link								>> xmd.ini
 @echo run											>> xmd.ini
 @echo exit											>> xmd.ini
 
