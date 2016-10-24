21f0269bc371815918c91751dd10b4c8a6510d6b
Marko Peshevski <markopesevski@gmail.com>
2016-10-24 22:43:02 +0200

    Alguns canvis a l'estructura de dintre de la pagina web, ara compilara al mfs tot el que hi ha dintre de la carpeta WEB/webpage

--- WEB/carregar_app.bat
+++ WEB/carregar_app.bat
@@ -19,18 +19,20 @@ REM crea la imatge per la pagina web
 :CREATE_MFS
 del "image.mfs"
 REM mfsgen -cvbf ./image.mfs 16 css index.html
-mfsgen -cvbf ./image.mfs 16 index.html
+mfsgen -cvbf ./image.mfs 16 .\webpage\*
 GOTO DO_BATCH
 
 REM si ha detectat el ISE correctament i la imatge ja ha estat creada
 :DO_BATCH
+del download.link
+mklink download.link "..\SDK\SDK_Export\lwip_example_hw_platform\download.bit"
 del "configuracio_batch_impact.cmd"
-@echo setMode -bscan																	>  configuracio_batch_impact.cmd
-@echo setCable -target "digilent_plugin DEVICE=SN:000000000000 FREQUENCY=-1"			>> configuracio_batch_impact.cmd
-@echo identify																			>> configuracio_batch_impact.cmd
-@echo assignfile -p 1 -file ..\SDK\SDK_Export\lwip_example_hw_platform\download.bit		>> configuracio_batch_impact.cmd
-@echo program -p 1																		>> configuracio_batch_impact.cmd
-@echo quit																				>> configuracio_batch_impact.cmd
+@echo setMode -bscan															>  configuracio_batch_impact.cmd
+@echo setCable -target "digilent_plugin DEVICE=SN:000000000000 FREQUENCY=-1"	>> configuracio_batch_impact.cmd
+@echo identify																	>> configuracio_batch_impact.cmd
+@echo assignfile -p 1 -file download.link										>> configuracio_batch_impact.cmd
+@echo program -p 1																>> configuracio_batch_impact.cmd
+@echo quit																		>> configuracio_batch_impact.cmd
 GOTO CONNECT
 
 REM un cop ja ha carregat el bootloop a la Spartan6
@@ -52,3 +54,8 @@ del "xmd.ini"
 xmd
 
 :EOF
+del "raw_apps.link"
+del "download.link"
+del "image.mfs"
+del "xmd.ini"
+del "configuracio_batch_impact.cmd"
--- WEB/configuracio_batch_impact.cmd
+++
@@ -1,6 +0,0 @@
-setMode -bscan																	
-setCable -target "digilent_plugin DEVICE=SN:000000000000 FREQUENCY=-1"			
-identify																			
-assignfile -p 1 -file ..\SDK\SDK_Export\lwip_example_hw_platform\download.bit		
-program -p 1																		
-quit																				
